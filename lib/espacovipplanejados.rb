fb51bf8c0d30df700dd99f5fcc37a230e479dd35
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2023-06-01 15:37:30 -0300

    Git init

---
+++ lib/espacovipplanejados.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "espacovipplanejados/version"
+
+module Espacovipplanejados
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/espacovipplanejados/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Espacovipplanejados
+  VERSION = "0.1.0"
+end
