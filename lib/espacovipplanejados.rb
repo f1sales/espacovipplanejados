68a8d5692aba9584aba768af2430228cd0643f81
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2023-06-01 16:17:30 -0300

    Requires files

--- lib/espacovipplanejados.rb
+++ lib/espacovipplanejados.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require_relative 'espacovipplanejados/version'
+require 'f1sales_custom/parser'
+require 'f1sales_custom/source'
+require 'f1sales_custom/hooks'
 
 module Espacovipplanejados
   class Error < StandardError; end
