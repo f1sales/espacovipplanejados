9452825990ebc962c9348891968ef7bf03e23c3e
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2023-06-29 14:58:19 -0300

    F1API-830: Pinheiros filter

--- spec/hooks_spec.rb
+++ spec/hooks_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe F1SalesCustom::Hooks::Lead do
   let(:lead) do
     lead = OpenStruct.new
     lead.source = source
+    lead.product = product
     lead.message = ''
 
     lead
@@ -15,6 +16,13 @@ RSpec.describe F1SalesCustom::Hooks::Lead do
     source.name = 'Some Source'
 
     source
+  end
+
+  let(:product) do
+    product = OpenStruct.new
+    product.name = ''
+
+    product
   end
 
   let(:switch_source) { described_class.switch_source(lead) }
@@ -28,35 +36,47 @@ RSpec.describe F1SalesCustom::Hooks::Lead do
   context 'when lead is from Facebook' do
     before { source.name = 'Facebook - Espaço Vip Ambientes Planejados' }
 
-    context 'when customer choose the São Caetano' do
-      before { lead.message = 'em_qual_unidade_você_gostaria_de_ser_atendido(a)?: são_caetano' }
+    context 'when store come in message' do
+      context 'when customer choose the São Caetano' do
+        before { lead.message = 'em_qual_unidade_você_gostaria_de_ser_atendido(a)?: são_caetano' }
 
-      it 'returns source - São Caetano unit' do
-        expect(switch_source).to eq('Facebook - Espaço Vip Ambientes Planejados - São Caetano')
+        it 'returns source - São Caetano unit' do
+          expect(switch_source).to eq('Facebook - Espaço Vip Ambientes Planejados - São Caetano')
+        end
+      end
+
+      context 'when customer choose the Lapa unit' do
+        before { lead.message = 'em_qual_unidade_você_gostaria_de_ser_atendido(a)?: lapa' }
+
+        it 'returns source - Lapa' do
+          expect(switch_source).to eq('Facebook - Espaço Vip Ambientes Planejados - Lapa')
+        end
+      end
+
+      context 'when customer choose the Pinheiros unit' do
+        before { lead.message = 'em_qual_unidade_você_gostaria_de_ser_atendido(a)?: pinheiros' }
+
+        it 'returns source - Pinheiros' do
+          expect(switch_source).to eq('Facebook - Espaço Vip Ambientes Planejados - Pinheiros')
+        end
+      end
+
+      context 'when customer choose the Santana unit' do
+        before { lead.message = 'em_qual_unidade_você_gostaria_de_ser_atendido(a)?: santana' }
+
+        it 'returns source - Santana' do
+          expect(switch_source).to eq('Facebook - Espaço Vip Ambientes Planejados - Santana')
+        end
       end
     end
 
-    context 'when customer choose the Lapa unit' do
-      before { lead.message = 'em_qual_unidade_você_gostaria_de_ser_atendido(a)?: lapa' }
+    context 'when lead store come in product' do
+      context 'when customer choose the Loja Pinheiros' do
+        before { product.name = 'Loja Pinheiros - Móveis Planejados até 50%off * 26.06.2023' }
 
-      it 'returns source - Lapa' do
-        expect(switch_source).to eq('Facebook - Espaço Vip Ambientes Planejados - Lapa')
-      end
-    end
-
-    context 'when customer choose the Pinheiros unit' do
-      before { lead.message = 'em_qual_unidade_você_gostaria_de_ser_atendido(a)?: pinheiros' }
-
-      it 'returns source - Pinheiros' do
-        expect(switch_source).to eq('Facebook - Espaço Vip Ambientes Planejados - Pinheiros')
-      end
-    end
-
-    context 'when customer choose the Santana unit' do
-      before { lead.message = 'em_qual_unidade_você_gostaria_de_ser_atendido(a)?: santana' }
-
-      it 'returns source - Santana' do
-        expect(switch_source).to eq('Facebook - Espaço Vip Ambientes Planejados - Santana')
+        it 'returns source - São Caetano unit' do
+          expect(switch_source).to eq('Facebook - Espaço Vip Ambientes Planejados - Pinheiros')
+        end
       end
     end
   end
